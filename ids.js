aebbdf3302dd80ea9474deb1fba63d33c8bfbca4
pesutak <pesutak@users.noreply.github.com>
2020-08-29 11:04:32 +0000

    deploy: 069005afbc022f3895d7818d1d242e6abee1cff1

--- ids.js
+++
@@ -1,9 +0,0 @@
-const { GH_CLIENT_ID, GH_CLIENT_SECRET, GH_CB_URL} = process.env
-
-module.exports = {
-	github: {
-		clientID: GH_CLIENT_ID,
-		clientSecret: GH_CLIENT_SECRET,
-		callbackURL: GH_CB_URL
-	}
-}
